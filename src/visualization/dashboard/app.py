f4b06e81c68455fae880df046075ea21718281eb
dalvanribeiro <dalvanra@hotmail.com>
2024-05-27 23:40:24 -0300

    Mapa de cachoeiro com os casos de dengue

--- src/visualization/dashboard/app.py
+++ src/visualization/dashboard/app.py
@@ -1,22 +1,67 @@
-from shared import gdf, df
+from shared import gdf, df, gdf_map, geojson_data
 import matplotlib.pyplot as plt
 import seaborn as sns
 from shiny.express import render, ui
+import folium
+from folium.plugins import MarkerCluster
+from folium.plugins import HeatMap
+import branca
+import numpy as np
+
 
 ui.page_opts(title="Dengue Cachoeiro", fillable=True)
 
-with ui.layout_columns():
-    with ui.card(full_screen=True):
-        ui.card_header("Mapa de Cachoeiro de Itapemirim")
-        @render.plot
-        def mapa():
-            fig, ax = plt.subplots(figsize=(10, 10))
-            gdf.plot(ax=ax)
-            return fig
+with ui.card():
+    ui.card_header("Mapa de Cachoeiro de Itapemirim")
+    @render.ui
+    def mapa():
+
+        m = folium.Map(location=[-20.838016, -41.132593], zoom_start=10, width='100%', height='60%')
+
+
+
+        tooltip = folium.GeoJsonTooltip(
+            fields=[ 'NOMES', 'AREA_KM2', 'Casos'],
+            aliases=['Nome: ', 'Àrea(Km²): ', 'Casos de dengue: '],
+            localize=True,
+            sticky=False,
+            labels=True,
+            style="""
+                background-color: #F0EFEF;
+                border: 2px solid black;
+                border-radius: 3px;
+                box-shadow: 3px;
+            """,
+            max_width=800,
+        )
+
+        colormap = branca.colormap.linear.OrRd_09.scale(
+            vmin=gdf_map["Casos"].quantile(0.0),
+            vmax=gdf_map["Casos"].quantile(1),
+            #colors=["red", "orange", "lightblue", "green", "darkgreen"],
+            #caption="State Level Median County Household Income (%)",
+        )
+
+        # Adiciona os limites dos estados a partir do GeoDataFrame
+        folium.GeoJson(
+            gdf_map,
+            name='geojson',
+            style_function=lambda x: {
+                "fillColor": colormap(x["properties"]["Casos"])
+                if x["properties"]["Casos"] is not None
+                else "transparent",
+                "color": "black",
+                "fillOpacity": 1,
+            },
+            tooltip=tooltip
+        ).add_to(m)
+        for feature in geojson_data['features']:
+            coordinates = [feature['properties']['latitude'], feature['properties']['longitude']]
+            folium.Marker(location=coordinates, popup=str(feature['properties']['nm_fantas'] + '\n'+ str(coordinates))).add_to(m)
         
-    with ui.card(full_screen=True):
-        ui.card_header("Casos de dengue por bairro")
-        @render.plot
-        def grafico():
-            sns.barplot(df, x="casos", y="local")
-            plt.yticks(fontsize=5)
+        
+        folium.LayerControl().add_to(m)
+        # Adiciona a camada de controle
+        
+        # Renderiza o mapa como HTML e passa para o Shiny
+        return ui.HTML(m._repr_html_())
--- src/visualization/dashboard/shared.py
+++ src/visualization/dashboard/shared.py
@@ -2,7 +2,21 @@ from pathlib import Path
 
 import geopandas as gpd
 import pandas as pd
+import json
+import numpy as np
 
 app_dir = Path(__file__).parent
-gdf = gpd.read_file('SHP\SHP - Comunidades Urbanas e Rurais\Limites de Comunidades Urbanas e Rurais de Cachoeiro de Itapemirim.shp')
+gdf_map = gpd.read_file('SHP\SHP - Comunidades Urbanas e Rurais\Limites de Comunidades Urbanas e Rurais de Cachoeiro de Itapemirim.shp')
+# gdf_map['dengue'] = [0 for _ in range(len(gdf_map))]
+
 df = pd.read_csv('dengue.csv')
+gdf_map = gdf_map.merge(df, on='NOMES', how='left', )
+
+
+print(gdf_map)
+
+with open('GeoJSON - Unidades de Saude\GeoJSON - Unidades de Saude.geojson', 'r') as f:
+        geojson_data = json.load(f)
+
+df_points = pd.DataFrame([(point['properties']['longitude'], point['properties']["latitude"], point['properties']['nm_fantas']) for point in geojson_data['features']], columns=['longitude', 'latitude', 'nm_ubs'])
+gdf = gpd.read_file('GeoJSON - Unidades de Saude\GeoJSON - Unidades de Saude.geojson')
